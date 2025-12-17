3a5953425bcfda635634c736694abcb93023745e
Mekost <wiktorkostka@gmail.com>
2025-12-17 10:20:44 +0100

    feat: add service

---
+++ server/src/main/java/pl/edu/agh/to/kotospring/server/services/interfaces/ExperimentExecutionService.java
@@ -0,0 +1,9 @@
+package pl.edu.agh.to.kotospring.server.services.interfaces;
+
+import pl.edu.agh.to.kotospring.server.models.QueueData;
+
+public interface ExperimentExecutionService {
+
+    void partStatusManager(QueueData queueData);
+    void runExperimentPart(QueueData queueData);
+}
