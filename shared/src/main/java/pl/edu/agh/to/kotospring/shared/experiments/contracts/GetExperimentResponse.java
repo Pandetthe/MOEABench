6dfef6fa6d522dc5b5324157a878f16b5209b971
Jakub Jurczyk <pandetthe@gmail.com>
2026-01-06 16:02:01 +0100

    fix: incomplete import

--- shared/src/main/java/pl/edu/agh/to/kotospring/shared/experiments/contracts/GetExperimentResponse.java
+++ shared/src/main/java/pl/edu/agh/to/kotospring/shared/experiments/contracts/GetExperimentResponse.java
@@ -3,7 +3,6 @@ package pl.edu.agh.to.kotospring.shared.experiments.contracts;
 import pl.edu.agh.to.kotospring.shared.experiments.ExperimentStatus;
 
 import java.time.OffsetDateTime;
-im
 import java.util.List;
 
 public record GetExperimentResponse(
